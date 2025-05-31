ece17c11f259eb992517daf77e95531bef5e4da8
nacholozanno <165487248+nacholozanno@users.noreply.github.com>
2025-05-31 00:28:37 -0300

    Update TP.pl

--- TP.pl
+++ TP.pl
@@ -51,6 +51,7 @@ plantas_companeras(rose, bamboo).
 %La base puede expandirse en el futuro, por lo que debe poder aceptar todos los casos.
 %TODOS los predicados deben ser inversibles y no puede utilizarse recursividad, salvo que se indique lo contrario.
 %Tareas:
+
 %1 Queremos poder preguntar sobre nuestras plantas si:
 %a. Son de tipo arbusto.
 %b. Florecen en primavera.
@@ -58,30 +59,32 @@ plantas_companeras(rose, bamboo).
 plantaArbusto(Planta) :-planta(Planta, tipo(arbusto)).
 florecePrimavera(Planta) :-planta(Planta, epoca(floracion, primavera)).
 plantaColor(Planta, Color) :-planta(Planta, color(Color)).
+
 %2 Sabemos que:
 %a. Las plantas que son arbustos y florecen en verano deben tener un sistema de riego especial.
 riegoEspecial(Planta) :-
     planta(Planta, tipo(arbusto)),
     planta(Planta, epoca(floracion, verano)).
+    
 %b. Las plantas rojas o amarillas atraen más insectos benéficos.
 atraeInsectos_beneficos(Planta) :-
     planta(Planta, color(rojo)).
-
 atraeInsectos_beneficos(Planta) :-
     planta(Planta, color(amarillo)).
+    
 %c. Las plantas que no son de tipo flor son consideradas altas.
 esAlta(Planta) :-
     planta(Planta, tipo(Tipo)),
     Tipo \= flor.
 %Realizar el predicado que nos permita agregar este conocimiento y el punto 1 no deba modificarse.
+
 %3 Se necesita conocer el conjunto de todas las plantas que son cortas y de tipo flor.
 plantaCortaFlor(Planta) :-
     planta(Planta, altura(corta)),
-    planta(Planta, tipo(flor)).    
-esPlantaCortaFlor(Planta, Conjunto) :-
-    plantaCortaFlor(Planta),
-    conjuntoCortoFlor(Conjunto),
-    member(Planta, Conjunto).
+    planta(Planta, tipo(flor)).
+plantasCortasFlorConjunto(Plantas) :-
+    findall(Planta, plantaCortaFlor(Planta), Plantas).
+    
 %4 Ahora debemos agregar las pistas obtenidas durante las observaciones. Por ejemplo:
 pista(arbol_rojo, tipo(arbusto)).
 pista(arbol_rojo, altura(media)).
@@ -92,6 +95,7 @@ coincideConPistas(Planta, Observador) :-
     planta(Planta, _),
     forall(pista(Observador, Caracteristica),
            planta(Planta, Caracteristica)).
+           
 %5 Finalmente, queremos saber si una planta está atrayendo más visitas que su compañera.
 %Para ello, la cantidad de pistas que cumple debe ser mayor que la de las plantas compañeras.
 planta_atrae_mas_visitas(Planta, Observador) :-
