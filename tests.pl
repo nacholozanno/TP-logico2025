ebe6ca8152cb42c376949bcbdf1324c2b9edc279
nacholozanno <165487248+nacholozanno@users.noreply.github.com>
2025-05-31 01:26:05 -0300

    Update tests.pl

--- tests.pl
+++ tests.pl
@@ -1,40 +1,81 @@
 :-include("TP").
 
-%tests
+% Punto 1: Consultas básicas sobre plantas
 :-begin_tests(punto1).
+
 test(planta_arbusto, set(X == [rose])) :-
     plantaArbusto(X).
+
 test(floracion_primavera, set(X == [rose, tulip, orchid])) :-
     florecePrimavera(X).
+
 test(planta_color_amarillo, set(X == [tulip, sunflower])) :-
     plantaColor(X, amarillo).
+
+test(planta_color_verdes, set(X == [cactus, fern, bamboo, ivy])) :-
+    plantaColor(X, verdes).
+
 :-end_tests(punto1).
 
+
+% Punto 2: Reglas sobre riego, insectos, y altura según tipo
 :-begin_tests(punto2).
-test(riego_especial, fail) :-
-    riegoEspecial(_).
-test(insectos_beneficos, set(X == [rose, tulip, sunflower])) :-
-    atraeInsectos_beneficos(X).
-test(es_alta, set(X == [rose, cactus, fern, bamboo, ivy])) :-
-    esAlta(X).
+
+test(arbustos_florecen_verano, set(X == [])) :-
+    florecenEnVerano(X).
+
+test(atrae_insectos_beneficos, set(X == [rose, tulip, sunflower])) :-
+    atraeInsectosBeneficos(X).
+
+test(plantas_no_flor_altas, set(X == [rose, cactus, fern, bamboo, ivy])) :-
+    plantasNoFlorAlta(X).
+
 :-end_tests(punto2).
 
+
+% Punto 3: Conjunto de plantas cortas y de tipo flor
 :-begin_tests(punto3).
-test(corta_flor, set(X == [lily, tulip, orchid])) :-
+
+test(plantas_cortas_flor, set(X == [lily, tulip, orchid])) :-
     plantaCortaFlor(X).
+
+test(conjunto_plantas_cortas_flor, true(Plantas == [lily, tulip, orchid])) :-
+    plantasCortasFlorConjunto(Plantas).
+
 :-end_tests(punto3).
 
-:-begin_tests(punto4)
-test(coincide_con_pistas, true) :-
-    coincideConPistas(rose, arbol_rojo).
-test(coincide_con_pistas_negativo, fail) :-
-    coincideConPistas(tulip, arbol_rojo).
+
+% Punto 4: Coincidencia entre planta y observador
+:-begin_tests(punto4).
+
+test(coincide_con_pistas_ok, set(X == [rose])) :-
+    coincideConPistas(X, arbol_rojo).
+
+test(coincide_con_pistas_fail, fail) :-
+    coincideConPistas(lily, arbol_rojo).
+
 :-end_tests(punto4).
 
-:-begin_tests(punto5).
-test(mas_visitas, true) :-
-    planta_atrae_mas_visitas(rose, arbol_rojo).
 
-test(no_mas_visitas, fail) :-
-    planta_atrae_mas_visitas(lily, arbol_rojo).
+% Punto 5: Comparación entre plantas compañeras según pistas
+:-begin_tests(punto5).
+
+% Agregamos pistas para este test
+:- dynamic pista/2.
+pista(visitante1, tipo(flor)).
+pista(visitante1, color(amarillo)).
+pista(visitante1, altura(corta)).
+
+test(pistas_cumple_tulip, true(C == 3)) :-
+    pistasCumple(tulip, visitante1, C).
+
+test(pistas_cumple_sunflower, true(C == 2)) :-
+    pistasCumple(sunflower, visitante1, C).
+
+test(tulip_mas_visitas_que_sunflower, set(X == [tulip])) :-
+    plantaMasVisitas(X, visitante1).
+
+test(sunflower_mas_visitas_fail, fail) :-
+    plantaMasVisitas(sunflower, visitante1).
+
 :-end_tests(punto5).
